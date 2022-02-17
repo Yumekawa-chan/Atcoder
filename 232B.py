a3cf0a24e84d341a0dfb1baaafb1206936869a60
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2022-02-17 12:56:59 +0900

    no completed

---
+++ 232B.py
@@ -0,0 +1,22 @@
+# s = input()
+# t = input()
+
+# s = list(s)
+# t = list(t)
+
+# for i in range(len(s)):
+#      s[i] = ord(s[i])
+#      t[i] = ord(t[i])
+
+# dif = t[0] - s[0]
+# print(dif)
+
+# if dif != 0:
+#      for i in range(len(s)):
+#           s[i] += s[i] + dif
+
+# for i in range(len(s)):
+#      s[i] = s[i]%97
+#      t[i] = t[i]%97
+
+# print(s,t)
